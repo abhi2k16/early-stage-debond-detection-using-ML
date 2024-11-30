108213952e86bffb983e79020a950a3a6256291c
Abhijeet Kumar <31257269+abhi2k16@users.noreply.github.com>
2024-11-30 21:46:14 +0530

    Update Dam_cat_class.py

--- Dam_cat_class.py
+++ Dam_cat_class.py
@@ -32,54 +32,41 @@ for i in range(177):
         T_class[i] = 4
 # Change the class value into one_hot encoder 
 T_class = F.one_hot(T_class % 4)
-
 X_train, X_test, y_train, y_test = train_test_split(X_Feature, T_class, test_size=0.1, random_state=42)
-
 # checking device
 device='cuda' if torch.cuda.is_available() else 'cpu'
 print(device)
-
 # convert ndarray into torch tensor
 X_train_tensor = torch.from_numpy(X_train).float().to(device)
 X_test_tensor = torch.from_numpy(X_test).float().to(device)
-
-
 class nn_model(nn.Module):
     def __init__(self):
         super(nn_model, self).__init__()
         self.fc1 = nn.Linear(41,20)
         self.fc2 = nn.Linear(20,10)
         self.fc3 = nn.Linear(10,4)
-
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
-
 # Instantiate a neural network
 model = nn_model().to(device)
-
 from torch.optim import Adam
- 
 # Define the loss function with Classification Cross-Entropy loss and an optimizer with Adam optimizer
 loss_fn = nn.CrossEntropyLoss()
 optimizer = Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
-
 # Train The model on training data
 from torch.autograd import Variable
-
 # Function to save the model
 def saveModel():
     path = './dam_cat_classification.pth'
     torch.save(model.state_dict(), path)
-
 # Function to test the model with the test dataset and print the accuracy
 def testAccuracy():
     model.eval()
     accuracy = 0.0
     total = 0.0
-
     with torch.no_grad():
         for data in test_loadder:
             inputs, labels = data
@@ -92,10 +79,8 @@ def testAccuracy():
     accuracy = (100 * accuracy / total)
     return accuracy
 # training function
-
 def train(epochs):
     best_accuracy = 0.0
-
     for epoch in range(epochs):
         running_loss = 0.0
         running_acc = 0.0
@@ -117,16 +102,10 @@ def train(epochs):
         if accuracy > best_accuracy:
             saveModel()
             best_accuracy = accuracy
-
-
-
-
-
 batch_size = 159
 n_itr = 5
 batch_size  = batch_size
 training_loss = []
-
 for epoch in range(n_itr):
     running_loss = 0
     for i in range(int(X_train_tensor.size()[0]/batch_size)):
@@ -141,7 +120,6 @@ for epoch in range(n_itr):
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-
         running_loss += loss.item()
     training_loss.append(running_loss/(X_train_tensor.size()[0]/10))
     if epoch % 10 == 0:
@@ -153,7 +131,6 @@ plt.legend(loc='best')
 plt.xlabel('Epochs')
 plt.ylabel('Training loss')
 plt.show()
-
 # Test
 X_data=X_train_tensor
 y_h=model(X_data)
@@ -166,8 +143,6 @@ fig = plt.figure()
 plt.plot(y_pred,'g.')
 plt.plot(y_act,'b.')
 plt.show()
-
-
 ############################### SVR ###########################################
 # from sklearn.svm import SVR
 
